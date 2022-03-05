b94328e8f68c7e5062d9d23c0c6b6429bb6f5c3c
Ivonne Delgado <32944129+ivonne06@users.noreply.github.com>
2022-03-04 19:17:54 -0600

    Revert "Revert "Charles""

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php 
-                    
-
-
-                     ?>
+<?php
+require("eventos.php");
+?>
 
 <!DOCTYPE html>
 <html>
@@ -11,28 +9,36 @@
 	<title>Agenda</title>
 </head>
 <body>
-
-
-     <form action="var.php" method="POST" class="formulario__login"  name="form1" id="form">
+    <form action="index.php" method="POST" class="formulario__login"  name="form1" id="form">
                         
         <input type="text" name="titulo" placeholder="Titulo del evento">
         <input type="date" id="fecha" name="fecha" value="" min="" max="">
 
-        <input type="text" name="descrip" placeholder="descripcion">
+        <input type="text" name="descripcion" placeholder="descripcion">
         <input type="submit" name="agregar" value="guardar evento">
         
-    </form>               
-                        
-                    
+        <!-- <a href="index.html" id="return"><input type="submit" name="" value="regresar" id="ret"></a> -->
+    </form>     
+    
+    <?php
+    if(isset($_POST['agregar'])){
+        if ($_POST) {
+            $titulo = isset($_POST["titulo"]) ? $_POST["titulo"] : '';
+            $fecha = isset($_POST["fecha"]) ? $_POST["fecha"] : '';
+            $descripcion = isset($_POST["descripcion"]) ? $_POST["descripcion"] : '';
 
-<?php 
+            $event = new Eventos ($titulo,$fecha,$descripcion); 
+            //$event2 = new Eventos ($titulo,$fecha,$descripcion);  
+            $ListaEventos = $event->Guardar_Datos();
 
+            foreach($ListaEventos as $key => $value){
+                echo $value;
+                echo "<br>";
+            }
 
-                  
-                   
-
-
-
-                     ?>
+        }
+            //var_dump($ListaEventos);
+    }
+    ?>
 </body>
 </html>
